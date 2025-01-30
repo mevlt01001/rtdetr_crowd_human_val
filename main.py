402a7e43a1545e0888ffaed9343ac1638f097417
mevlt01001 <mevlutbasaran01@gmail.com>
2025-01-30 18:40:56 +0300

    latencies updated

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ with open(annotation_file_path, 'r') as file:
 
 images_path = 'CrowdHuman_val/Images/'
 
-time_data = []
+total_ms_data = []
+preproces_ms_data = []
+inference_ms_data = []
+postprocess_ms_data = []
 fps_data = []
 iou_data = []
 confision_matrix_data = []
@@ -40,15 +43,23 @@ for i, annotation in enumerate(annotations):
         memory_info = psutil.virtual_memory()# RAM 
         
         used_ram = memory_info.used / 1e6
-        ms = result[0].speed['inference']
-        fps = 1000 / ms
+
+        preproces_ms = result[0].speed['preprocess']
+        inference_ms = result[0].speed['inference']
+        postprocess_ms = result[0].speed['postprocess']
+        total_ms = preproces_ms + inference_ms + postprocess_ms
+        
+        fps = 1000 / total_ms
         power_values = [float(x) for x in _result.decode('utf-8').strip().split('\n')]
 
-        time_data.append(ms)
+        total_ms_data.append(total_ms)
+        preproces_ms_data.append(preproces_ms)        
+        inference_ms_data.append(inference_ms)        
+        postprocess_ms_data.append(postprocess_ms)
         fps_data.append(fps)
         watt_data.append(sum(power_values) / len(power_values))
         gpu_data.append(allocated_memory)
-        ram_data.append(used_ram)
+        ram_data.append(used_ram)        
 
 
         mask = result[0].boxes.data[:, -1] == 0
@@ -85,20 +96,27 @@ recall = data['recall'].to_numpy()
 precision = data['precision'].to_numpy()
 
 # average calculation
+
 average_fps = np.mean(fps_data)
-average_latency = np.mean(time_data)
+average_preprocess_latency = np.mean(preproces_ms_data)
+average_inference_latency = np.mean(inference_ms_data)
+average_postprocess_latency = np.mean(postprocess_ms_data)
+average_total_latency = np.mean(total_ms_data)
 avg_gpu_usage = np.mean(gpu_data)
 avg_ram_usage = np.mean(ram_data)
 avg_watt_usage = np.mean(watt_data)
-average_precision = np.trapz(precision, recall)
+average_precision = np.mean(precision)
 
 os.system("clear")
 print(f"Average FPS: {average_fps:.4f}")
-print(f"Average Latency: {average_latency:.4f} ms")
+print(f"Average Preprocess Latency: {average_preprocess_latency:.4f} ms")
+print(f"Average Inference Latency: {average_inference_latency:.4f} ms")
+print(f"Average Postprocess Latency: {average_postprocess_latency:.4f} ms")
+print(f"Average Total Latency: {average_total_latency:.4f} ms")
 print(f"Average GPU Usage: {avg_gpu_usage:.4f} MB")
 print(f"Average RAM Usage: {avg_ram_usage:.4f} MB")
-print(f"Average Watt Usage: {avg_watt_usage:.4f}")
-print(f"AP: {average_precision:.4f}")
+print(f"Average Watt Usage: {avg_watt_usage:.4f} Watt")
+print(f"Average Precision: {average_precision:.4f}")
 
 import matplotlib.pyplot as plt
 
